8fba7821663eec90f45e87d1c2478b51b593e726
rahiyansafz <rahiyansafin@gmail.com>
2022-05-04 01:53:24 +0600

    slider section / page added

--- src/LibComp/common/layout/body/body.tsx
+++ src/LibComp/common/layout/body/body.tsx
@@ -1,78 +1,10 @@
 import { Box } from "@mui/material";
-import React from "react";
+import Slider from "../../../../pages/slider";
 
 export default function Body() {
   return (
     <Box sx={{ p: 4 }}>
-      BODY
-      {/* <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi omnis
-        rerum, ipsam ipsa ducimus, reprehenderit odio dicta dolorem laborum
-        consectetur minus quo dolores a voluptates necessitatibus voluptate,
-        perferendis accusamus iure quae vel sequi cupiditate. Accusamus illum
-        soluta totam dolorum ut tempora dolore iusto, laborum sed voluptatem
-        enim, ratione velit odit officia a sequi animi. Ullam laborum maiores
-        ipsa deleniti vel obcaecati consequuntur quidem rerum voluptates, earum
-        quas ea commodi nam. Unde assumenda temporibus, qui labore officiis ex
-        et mollitia ipsam exercitationem vel necessitatibus ab pariatur eaque
-        autem corporis a accusantium quaerat. Exercitationem provident
-        consequatur culpa, voluptatibus suscipit debitis delectus quibusdam
-        corrupti quam, quidem, officia quaerat labore fugit? Veritatis
-        repellendus sequi eius, non sapiente explicabo ullam necessitatibus aut
-        temporibus qui alias totam quam eum ipsa vero nostrum rerum ducimus
-        ipsum! Magni a sit modi minima quae adipisci ipsa blanditiis dolor
-        perspiciatis iste quam non voluptate accusantium repellendus fuga
-        necessitatibus doloremque eaque iusto, accusamus at tempora maiores
-        sequi qui temporibus? Reiciendis itaque voluptatem voluptates cupiditate
-        impedit animi ducimus fugit suscipit ipsa corrupti? Non nam nemo
-        dignissimos nihil quisquam dolor asperiores quidem! Non hic tempora at
-        odit commodi laboriosam magni ad similique porro. Facilis id eos
-        accusamus iusto dolores est error mollitia culpa tempora quia
-        praesentium autem voluptatibus porro magni consequuntur laboriosam
-        voluptates, et nam dicta, nisi earum accusantium in sed. Quos deleniti
-        earum sunt maxime rem officiis esse? Quae tempore neque, cum labore
-        necessitatibus distinctio, beatae ad sed nam libero, atque earum
-        molestias id est repudiandae? Veniam rerum sint possimus saepe esse
-        repudiandae quis repellat. Sapiente, officia ratione dignissimos
-        pariatur molestias odit reiciendis saepe, minus rem, consectetur atque.
-        Impedit accusantium itaque tempora voluptatibus velit voluptate
-        obcaecati mollitia nisi, nam illo ad dolor commodi eos doloribus
-        distinctio eius sequi exercitationem id suscipit iste? Rerum, eius
-        pariatur ex repellat recusandae tempore expedita in beatae cupiditate
-        magni dolorum minima facere impedit illum! Nobis dignissimos beatae nisi
-        nesciunt libero, quisquam temporibus consequatur magnam repellendus
-        suscipit commodi eligendi molestiae explicabo eum tenetur velit sapiente
-        illum. Delectus illo ab inventore aliquid qui eius exercitationem
-        quisquam, ipsa blanditiis sint facilis voluptate odit praesentium velit?
-        Esse delectus culpa a, commodi nobis voluptatum labore maxime ipsum
-        ullam sapiente tempora corrupti dicta odio, est laborum ducimus et quae
-        rem? Aspernatur aliquam necessitatibus, quibusdam nam cum, iure tempora
-        accusantium eveniet eos in sed vitae! Odio rem corrupti quas
-        perspiciatis quod temporibus ipsum, reiciendis ducimus saepe amet
-        quisquam voluptates laboriosam aspernatur dolorem qui est voluptatum
-        deserunt sit, deleniti dolor sunt numquam? Esse officiis harum qui velit
-        commodi quasi maiores, omnis eveniet nisi officia dicta cupiditate
-        libero, accusamus nobis! Delectus eveniet aut, ipsa unde, cumque quis
-        voluptatem quisquam officia sint aliquam ab obcaecati? Delectus
-        architecto non autem, dolores nisi quia repellat fuga obcaecati
-        doloribus repudiandae aperiam odit cum amet eum ipsum aspernatur dolore,
-        laudantium quaerat mollitia! Harum, provident consectetur illum, modi
-        eaque quas nobis at autem quo iure id dolorem soluta quos! Facilis,
-        culpa sed deleniti alias consectetur maiores officiis nam perspiciatis.
-        Nesciunt error tempora nisi expedita dignissimos amet tenetur officia
-        ea, aliquam earum numquam dolorum beatae obcaecati ipsum quaerat,
-        distinctio reiciendis fuga eius consequuntur ducimus debitis? Facere
-        possimus quo expedita incidunt temporibus, asperiores corporis similique
-        harum eligendi repellat, et doloribus, ipsa magni nihil ducimus!
-        Aspernatur consequatur cum laboriosam magnam ab ullam quasi aut numquam
-        laudantium distinctio, corrupti ad explicabo voluptatem eaque odio optio
-        excepturi molestiae eos. Porro quaerat sunt magnam labore, perferendis
-        modi illo odio cumque? Iusto id, reiciendis laboriosam natus unde
-        commodi sint explicabo eaque, perferendis magnam esse, officia
-        consequatur animi doloribus nobis voluptas voluptatem minima maiores
-        consectetur voluptate accusantium quas nihil expedita dicta! Sunt
-        reprehenderit deserunt accusamus nemo vero amet cupiditate velit?
-      </p> */}
+      <Slider />
     </Box>
   );
 }
